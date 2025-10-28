7b8f9c0ec13a803c433106d41a6edafa6c2eb3f9
LAREDO LANCER <lancerajayi@yahoo.com>
2025-10-28 11:07:21 +0000

    Testing terraform deploy on AZ

---
+++ terraform/variables.tf
@@ -0,0 +1,24 @@
+variable "location" {
+  description = "Azure region"
+  default     = "UK South"
+}
+
+variable "resource_group_name" {
+  description = "Name of the resource group"
+  default     = "wordpress-rg"
+}
+
+variable "admin_username" {
+  description = "Admin username for VM"
+  default     = "azureuser"
+}
+
+variable "vm_size" {
+  description = "Size of the VM"
+  default     = "Standard_B2s"
+}
+
+variable "ssh_public_key_path" {
+  description = "Path to SSH public key"
+  default     = "~/.ssh/id_rsa.pub"
+}
