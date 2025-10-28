53c8f1d87c324b863b98b81913fc7817000b74c0
LAREDO LANCER <lancerajayi@yahoo.com>
2025-10-28 11:35:49 +0000

    Terra 7

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,12 @@
-resource "azurerm_resource_group" "rg" {
-  name     = var.resource_group_name
-  location = var.location
+# resource "azurerm_resource_group" "rg" {
+#   name     = var.resource_group_name
+#   location = var.location
+# }
+
+data "azurerm_resource_group" "rg" {
+  name = var.resource_group_name
 }
+
 
 resource "azurerm_virtual_network" "vnet" {
   name                = "wordpress-vnet"
